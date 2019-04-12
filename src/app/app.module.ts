89e803bb3251da03d48b9daf737080ebcac11c38
Luciano Marinho <nickmarinho@gmail.com>
2019-04-12 03:56:55 -0300

    Adicionando esqueleto do menu do topo e da esquerda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { TopmenuComponent } from './topmenu/topmenu.component';
+import { LeftmenuComponent } from './leftmenu/leftmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopmenuComponent,
+    LeftmenuComponent
   ],
   imports: [
     BrowserModule,
