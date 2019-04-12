70ade5988a601c46c8a71b78eee2928365d2e20a
LUCIANO MARINHO <nickmarinho@gmail.com>
2019-04-12 16:36:06 -0300

    Adicionando o tema e as dependencias dos Mat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule, MatMenuModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { TopmenuComponent } from './topmenu/topmenu.component';
 import { LeftmenuComponent } from './leftmenu/leftmenu.component';
-import { MatMenuModule } from '@angular/material/menu';
+import { TopmenuComponent } from './topmenu/topmenu.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,9 @@ import { MatMenuModule } from '@angular/material/menu';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatMenuModule
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
